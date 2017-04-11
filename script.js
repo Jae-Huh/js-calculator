ebd09fa4e229078fe956a21c54239442421b84ed
Jae Huh <jaeeunhuh@gmail.com>
2017-04-11 20:34:44 +1000

    Display numbers pressed and turn the string to number

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 var numButtons = document.getElementsByClassName('num')
 var equality = document.getElementById('equality')
 var display = document.getElementById('display')
-var numSentence = [];
+var currNumStr = ""
+var currNum = 0
+
 
 //displays numbers and
 for (var i = 0; i < numButtons.length; i++) {
   numButtons[i].addEventListener('click', function(event) {
-    display.innerHTML = event.target.textContent
-    numSentence.push(event.target.textContent)
+    currNumStr += event.target.textContent
+    display.innerHTML = currNumStr
+    currNum = parseFloat(currNumStr)
+
   })
 }
 
